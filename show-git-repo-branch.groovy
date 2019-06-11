1ecf1695c193976cf0c8123779cb9a20fcf9c6db
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-11 17:05:31 -0400

    Start using LdxJob class

--- show-git-repo-branch.groovy
+++ show-git-repo-branch.groovy
@@ -1,3 +1,10 @@
+class LdxJob {
+  def name
+  String gitRepo
+  String branch
+}
+List ldxJobs = []
+
 counter = 0
 jobs = Jenkins.instance.getItems(AbstractProject.class)
 println '>>>>>>>>>> Total jobs = ' + jobs.size()
@@ -5,16 +12,12 @@ def gitScmJobs = []
 
 List gitRepos = []
 jobs.each {
-  //println it.name.padRight(70,'.') + it.scm.properties['key'].padRight(60)  + '  ' + it.scm.properties['branches'] 
-  println it.name.padRight(80,'.') + it.scm.properties['key'].tokenize('/').takeRight(2).toString().padRight(60)  + '  ' + it.scm.properties['branches'] 
-  gitRepos << it.scm.properties['key'].tokenize('/').last()
-  //println it.name.padRight(70,'.') + ' has GitSCM with ' + it.scm.properties
+  ldxJobs << new LdxJob(name: it.name, gitRepo: it.scm.properties['key'].tokenize('/').last(), branch: it.scm.properties['branches'])
 }
 
 println '-' * 150
 println 'Unique git repos:'
 println '-' * 150
-
-gitRepos.unique().each { println it }
+ldxJobs.collect{ it.gitRepo }.unique().each{ println it }
   
 println 'END'
