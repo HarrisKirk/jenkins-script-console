58870875054a50d2c625385b0adb7b455a72220f
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-11 16:44:51 -0400

    Correctly gets the 125 projects

--- show-git-repo-branch.groovy
+++ show-git-repo-branch.groovy
@@ -1,22 +1,10 @@
 counter = 0
-jobs = Jenkins.instance.getAllItems()
+jobs = Jenkins.instance.getItems(AbstractProject.class)
 println '>>>>>>>>>> Total jobs = ' + jobs.size()
 def gitScmJobs = []
 
-for (job in jobs) {
-  try {
-	  if (job.scm instanceof hudson.plugins.git.GitSCM ){
-            gitScmJobs << job
-      }
-  } catch (Exception e) {
-    //   
-  }
-
-}
-
-println "*** Job Count = " + gitScmJobs.size()
 List gitRepos = []
-gitScmJobs.each {
+jobs.each {
   //println it.name.padRight(70,'.') + it.scm.properties['key'].padRight(60)  + '  ' + it.scm.properties['branches'] 
   println it.name.padRight(80,'.') + it.scm.properties['key'].tokenize('/').takeRight(2).toString().padRight(60)  + '  ' + it.scm.properties['branches'] 
   gitRepos << it.scm.properties['key'].tokenize('/').last()
