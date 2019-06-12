89c72203613a33b5b71ce289423ba2949a6c2759
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-12 09:37:55 -0400

    Includes downstream jobs

--- show-git-repo-branch.groovy
+++ show-git-repo-branch.groovy
@@ -4,6 +4,7 @@ class LdxJob {
   boolean isGit
   String gitRepo
   String branch
+  List downstreamProjects
 }
 List ldxJobs = []
 
@@ -15,21 +16,21 @@ def gitScmJobs = []
 List gitRepos = []
 jobs.each {
   if (it.scm instanceof hudson.scm.NullSCM) {
-    ldxJobs << new LdxJob(name: it.name, isGit: false, gitRepo: 'n/a', branch: '', isDisabled: it.disabled)
+    ldxJobs << new LdxJob(name: it.name, isGit: false, gitRepo: 'n/a', branch: '', isDisabled: it.disabled, downstreamProjects: it.getDownstreamProjects().collect{it.name})
   } else {
-    ldxJobs << new LdxJob(name: it.name, isGit: true, gitRepo: it.scm.properties['key'].tokenize('/').last(), branch: it.scm.properties['branches'], isDisabled: it.disabled)
+    ldxJobs << new LdxJob(name: it.name, isGit: true, gitRepo: it.scm.properties['key'].tokenize('/').last(), branch: it.scm.properties['branches'], isDisabled: it.disabled, downstreamProjects: it.getDownstreamProjects().collect{it.name})
   }
 }
 
 println '-' * 150
 println 'All Jobs:'
 println '-' * 150
-ldxJobs.each{ println it.name.padRight(50,'.') + it.gitRepo.padRight(50,'.') + (it.isDisabled ?: '') }
+ldxJobs.each{ println it.name.padRight(50,'.') + it.gitRepo.padRight(50,'.') + (it.isDisabled ?: '') + it.downstreamProjects }
 
 println '-' * 150
 println 'All non-git Jobs:'
 println '-' * 150
-ldxJobs.findAll{!it.isGit}.each{ println it.name.padRight(50,'.') + ' disabled ' + (it.isDisabled ?: '') }
+ldxJobs.findAll{!it.isGit}.each{ println it.name.padRight(50,'.') + ' disabled ' + (it.isDisabled ?: '') + it.downstreamProjects }
 
 println '-' * 150
 println 'Unique git repos:'
