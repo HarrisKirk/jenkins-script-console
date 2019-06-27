8e5e91be488b9e5cfd3f6d02db7f058a4100bb7e
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-27 16:51:26 -0400

    getAllItems()

--- show-jobs.groovy
+++ show-jobs.groovy
@@ -1,8 +1,9 @@
-    def jobs = Jenkins.instance.getItems(AbstractProject.class)
+    def jobs = Jenkins.instance.getAllItems(AbstractProject.class)
     println '>>>>>>>>>> Total jobs = ' + jobs.size()
 
     List gitRepos = []
     jobs.each {
-      println it.name
+      println it.getClass().getName()
     }
 
+	println ''
