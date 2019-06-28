4b066ae66b01192b1b93418941cf712caeb3c213
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-28 12:26:04 -0400

    Show the shell command lines - enhanced again

--- show-jobs.groovy
+++ show-jobs.groovy
@@ -1,20 +1,26 @@
     import hudson.tasks.Shell
 
+	String prettyPrint(String jobName) {
+      StringBuffer sb = new StringBuffer() 
+      sb << '=' * jobName.size()
+      sb << jobName
+      sb << '=' * jobName.size()
+      sb.toString()
+    }
+  
+
 	def jobs = Jenkins.instance.getItems(AbstractProject.class).findAll{ !it.disabled }
     println '>>>>>>>>>> Total jobs = ' + jobs.size()
 
     List gitRepos = []
     jobs.each {
       if (it instanceof Project) { 
-        println '>>>>>>>>>>>> Processing ' + it.name 
-        println ''
+        println prettyPrint(it.name)
         List builders = it.getBuilders() 
         builders.each {
           println "Shell command:"
           if (it instanceof Shell ) { println it.getContents().replaceAll("(?m)^", "\t") }
         } 
-      } else {
-         println '>>>>>>>>>>>> Ignoring ' + it.name 
-      }
+      } 
     }
 	println ''
