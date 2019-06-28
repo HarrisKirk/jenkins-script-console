6da450be8bbb79cec2c68317684737b71edfe55d
Harris Kirk <harris.kirk@lumiradx.com>
2019-06-28 11:29:44 -0400

    Show the shell command lines

--- show-jobs.groovy
+++ show-jobs.groovy
@@ -1,8 +1,15 @@
+    import hudson.tasks.Shell
+
     def jobs = Jenkins.instance.getItems(AbstractProject.class)
     println '>>>>>>>>>> Total jobs = ' + jobs.size()
 
     List gitRepos = []
     jobs.each {
-      if (it instanceof Project) { println it.getBuilders() }
+      if (it instanceof Project) { 
+        List builders = it.getBuilders() 
+        builders.each {
+          if (it instanceof Shell ) { println it.getContents() }
+        } 
+      }
     }
 	println ''
