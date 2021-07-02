6a5b6f25f8caea6a745715c4cdd4396439396e93
Harris Kirk <cjtkirk@protonmail.com>
2021-07-02 15:52:05 -0400

    Update kythera-job-lister.groovy

--- kythera-job-lister.groovy
+++ kythera-job-lister.groovy
@@ -1,11 +1,13 @@
+println '-' *80
+
 counter = 0
-jobs = Jenkins.instance.getItems()
+jobs = Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject)
 println "*** Job Count = " + jobs.size()
-for (job in jobs) {
-  if (job.class.name == 'org.jenkinsci.plugins.workflow.job.WorkflowJob') {
-	  println job.name.padRight(45,'.') + job.class.name.padRight(75,'.') + ' ' + job.getTypicalSCM()
-  } else {
-	  println job.name.padRight(45,'.') + job.class.name.padRight(75,'.') 
-  }
+job = jobs[0]
+for (gen in job.getItems()) {
+  println gen
 }
+
+
+
 return ''
