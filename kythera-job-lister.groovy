657fce6ffece135001635d78518b94d3746bd3ab
Harris Kirk <cjtkirk@protonmail.com>
2021-07-02 14:49:25 -0400

    Update kythera-job-lister.groovy

--- kythera-job-lister.groovy
+++ kythera-job-lister.groovy
@@ -2,7 +2,7 @@ counter = 0
 jobs = Jenkins.instance.getItems()
 println "*** Job Count = " + jobs.size()
 for (job in jobs) {
-  println ">>" + job.name
+  println job.name.padRight(55,'.') + job.class.name
 }
 
 return ''
